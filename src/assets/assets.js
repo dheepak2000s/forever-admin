f46706d387f34824169566c2ee360352d11180bf
DheepakS <sdheepak25@gmail.com>
2025-10-18 12:05:03 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,9 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
+export const url = "https://forever-backend-qadk.onrender.com"
+
+
 export const assets = {
     logo,
     add_icon,
